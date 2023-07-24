65a24dcfc8e6b0abf611d2077fadae67e1d9cd84
Bruno Gomes <brunoggdev@gmail.com>
2023-07-24 13:31:00 -0300

    selecao de menu implementada

--- js.js
+++ js.js
@@ -2,4 +2,14 @@ fetch('menu.html')
 .then(response => response.text())
 .then(html => {
     document.querySelector('aside').innerHTML = html;
+    
+    const pagina_atual = window.location.href.split('/').pop().split('.')[0];
+    const paginas = Array.from(document.getElementsByClassName('list-group-item'));
+
+    for (const pagina of paginas) {
+        if (pagina.innerText === pagina_atual) {
+            pagina.closest('.accordion-collapse').previousElementSibling.firstChild.nextElementSibling.click()
+            pagina.classList.add('bg-lava');
+        }
+    }
 })
