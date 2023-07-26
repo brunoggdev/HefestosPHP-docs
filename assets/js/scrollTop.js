5b500c321e2949034ca72bc6b13244ed04cca4a1
Bruno Gomes <brunoggdev@gmail.com>
2023-07-26 10:58:42 -0300

    scrolltop updated

--- assets/js/scrollTop.js
+++ assets/js/scrollTop.js
@@ -23,6 +23,9 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    window.scrollBy({
+        top: -window.scrollY,
+        left: 0,
+        behavior: 'smooth' // You can change this to 'auto' or omit it for an instant scroll
+      })
 }
