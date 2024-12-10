7aa0a9cc0c1300881e6b4ea130bf8f67ea776b5a
Bruno Gomes <brunoggdev@gmail.com>
2024-12-09 22:07:15 -0300

    menu updates

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -6,10 +6,23 @@ fetch('menu.html')
     const pagina_atual = window.location.href.split('/').pop().split('.')[0];
     const paginas = Array.from(document.getElementsByClassName('list-group-item'));
 
-    for (const pagina of paginas) {
-        if (pagina.innerText === pagina_atual) {
+    for (const pagina of paginas) {        
+        if (pagina.innerText.toLowerCase() === 'intro') {
+            pagina.classList.remove('list-group-item')
+        }
+
+
+        if (pagina.innerText.toLowerCase() === pagina_atual.toLowerCase()) {
+
+            if (pagina_atual.toLowerCase() === 'intro') {
+                pagina.style.color = '#f0f0f0';
+                pagina.style.backgroundColor = 'var(--color-lava-dim)';
+                continue;
+            }
+
             pagina.closest('.accordion-collapse').previousElementSibling.firstChild.nextElementSibling.click()
             pagina.classList.add('bg-lava');
+            return;
         }
     }
 })
